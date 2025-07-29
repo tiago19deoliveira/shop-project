4bbfef8e9a3f4e260da9b26fa9044292c0482654
tiago oliveira <tiago18deoliveira@gmail.com>
2025-07-29 00:23:34 -0400

    chore:adicionando react query

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,8 @@ import { router } from "./routes";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
@@ -12,7 +14,9 @@ export function App() {
         <ThemeProvider storageKey="shop-project-theme" defaultTheme="dark">
           <Toaster richColors />
           <Helmet titleTemplate="%s | shop.product" />
-          <RouterProvider router={router} />
+          <QueryClientProvider client={queryClient}>
+           <RouterProvider router={router} />
+          </QueryClientProvider>
         </ThemeProvider>
       </HelmetProvider>
     </>
