0aebdb1e47df3b500f87a7795f2e887a07e51009
tiago oliveira <tiago18deoliveira@gmail.com>
2025-07-29 00:07:50 -0400

    feat: configuração env

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,13 @@ import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/sing-up";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement : <NotFound/>,
     children: [
       { path: "/", element: <Dash /> },
       { path: "/orders", element: <Orders /> },
