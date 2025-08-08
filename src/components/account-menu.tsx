e641b4511fd2df0f8e078af8268145e139267a92
tiago oliveira <tiago18deoliveira@gmail.com>
2025-08-08 09:43:11 -0400

    requisição para o nome do estabelecimento e exibição do perfil completo

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -12,11 +12,16 @@ import { Button } from "./ui/button";
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
+import { getManageRestaurant } from "@/api/get-managed-shop";
 
 export function AccountMenu() {
  const {data: profile}= useQuery({
     queryFn: getProfile,
     queryKey:['profile']
+  })
+   const {data: maanagedRestaurante}= useQuery({
+    queryFn: getManageRestaurant,
+    queryKey:['managed-restaurant']
   })
 
 
@@ -28,14 +33,14 @@ export function AccountMenu() {
             variant="outline"
             className="flex select-none items-center gap-2"
           >
-            {profile?.name}
+            {maanagedRestaurante?.name}
             <ChevronDown className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end" className="w-56">
           <DropdownMenuLabel className="flex flex-col">
-            <span>Tiago O.</span>
-            <span>tiagodevp@gmail.com</span>
+            <span>{profile?.name}</span>
+            <span>{profile?.email}</span>
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>
