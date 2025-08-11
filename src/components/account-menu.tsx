6219869f0f5b6db76c80a1c162295066597c4df0
tiago oliveira <tiago18deoliveira@gmail.com>
2025-08-10 23:28:25 -0400

    feat: configurando env para adicionar delay, alterando ui para adicionar skeletons

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -13,17 +13,18 @@ import { Building, ChevronDown, LogOut } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManageRestaurant } from "@/api/get-managed-shop";
+import { Skeleton } from "./ui/skeleton";
 
 export function AccountMenu() {
- const {data: profile}= useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryFn: getProfile,
-    queryKey:['profile']
-  })
-   const {data: maanagedRestaurante}= useQuery({
-    queryFn: getManageRestaurant,
-    queryKey:['managed-restaurant']
-  })
-
+    queryKey: ["profile"],
+  });
+  const { data: maanagedRestaurante, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryFn: getManageRestaurant,
+      queryKey: ["managed-restaurant"],
+    });
 
   return (
     <div>
@@ -33,14 +34,30 @@ export function AccountMenu() {
             variant="outline"
             className="flex select-none items-center gap-2"
           >
-            {maanagedRestaurante?.name}
+            {isLoadingManagedRestaurant ? (
+              <Skeleton className="h-4 w-40" />
+            ) : (
+              maanagedRestaurante?.name
+            )}
             <ChevronDown className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end" className="w-56">
           <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32"/>
+                <Skeleton className="h-2 w-32"/>
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+              </>
+            )}
+
             <span>{profile?.name}</span>
-            <span>{profile?.email}</span>
+            <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>
@@ -51,9 +68,8 @@ export function AccountMenu() {
             <LogOut className="text-rose-500 dark:text-rose-400" />
             <span>Sair</span>
           </DropdownMenuItem>
-        </DropdownMenuContent> 
+        </DropdownMenuContent>
       </DropdownMenu>
     </div>
   );
 }
- 
