88a4f860197d5b2c7154a8d7155644a73f345a7c
tiago oliveira <tiago18deoliveira@gmail.com>
2025-09-16 23:01:44 -0400

    feat: adicionando arquivo de teste para o order status

--- src/components/order-status.spec.tsx
+++ src/components/order-status.spec.tsx
@@ -1,4 +1,64 @@
-test('1 + 1 equals 2', () =>{
-    expect(1 + 1).toEqual(2)
-}
-)
+import { OrderStatus } from "./order-status"
+import {render} from "@testing-library/react"
+
+describe("OrderStatus", () => {
+  it("should display the right text when orde status is pending", () => {
+    const { getByTestId } = render(<OrderStatus status="pending" />);
+     /* PEnDING */
+    let badgeDot = getByTestId("badge-pending");
+    let badgeText = getByTestId("badge-status");
+
+    expect(badgeDot).toHaveClass("bg-slate-400");
+    expect(badgeText).toHaveTextContent("Pendente");
+
+  });
+
+  it("should display the right text when orde status is canceled", () => {
+        /*CANCELED*/
+    const { getByTestId } = render(<OrderStatus status="canceled" />);
+
+    let badgeDot = getByTestId("badge-canceled");
+    let badgeText = getByTestId("badge-status");
+
+    expect(badgeDot).toHaveClass("bg-rose-500");
+    expect(badgeText).toHaveTextContent("Cancelado");
+
+  });
+
+    it("should display the right text when orde status is delivering", () => {
+        /*DELIVERING*/
+    const { getByTestId } = render(<OrderStatus status="delivering" />);
+    
+    let badgeDot = getByTestId("badge-delivering");
+    let badgeText = getByTestId("badge-status");
+
+    expect(badgeDot).toHaveClass("bg-amber-500");
+    expect(badgeText).toHaveTextContent("Em entrega");
+
+  });
+
+//     it("should display the right text when orde status is processing", () => {
+//         /*PROCESSING*/
+//     const { getByTestId } = render(<OrderStatus status="processing" />);
+    
+//     let badgeDot = getByTestId("badge-processing");
+//     let badgeText = getByTestId("badge-status");
+
+//     expect(badgeDot).toHaveClass("bg-amber-500");
+//     expect(badgeText).toHaveTextContent("Em Preparo");
+
+//   });
+
+    it("should display the right text when orde status is delivered", () => {
+        /*DELIVERED*/
+    const { getByTestId } = render(<OrderStatus status="delivered" />);
+    
+    let badgeDot = getByTestId("badge-delivered");
+    let badgeText = getByTestId("badge-status");
+
+    expect(badgeDot).toHaveClass("bg-emerald-500");
+    expect(badgeText).toHaveTextContent("Entregue");
+
+  });
+
+});
--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -16,20 +16,20 @@ export function OrderStatus({status}: OrderStatusProps) {
     return (
           <div className="flex items-center gap-2">
            {status ==='pending' && (
-               <span className="h-2 w-2 rounded-full bg-slate-400" />
+               <span data-testid="badge-pending" className="h-2 w-2 rounded-full bg-slate-400" />
            )}
            {status ==='canceled' && (
-               <span className="h-2 w-2 rounded-full bg-rose-500" />
+               <span data-testid="badge-canceled" className="h-2 w-2 rounded-full bg-rose-500" />
            )}
            {status ==='delivered' && (
-               <span className="h-2 w-2 rounded-full bg-emerald-500" />
+               <span  data-testid="badge-delivered" className="h-2 w-2 rounded-full bg-emerald-500" />
            )}
 
            {['processing', 'delivering'].includes(status) && (
-            <span className="h-2 w-2 rounded-full bg-amber-500" />
+            <span  data-testid="badge-delivering" className="h-2 w-2 rounded-full bg-amber-500" />
            )}
 
-            <span className="font-medium text-muted-foreground"> {orderStatusMap[status]}</span>
+            <span data-testid="badge-status" className="font-medium text-muted-foreground"> {orderStatusMap[status]}</span>
           </div>
     )
 }
